95319edd571340e2ac6b946d303dd710c6bd9f1d
cuifutao <cuifutao@sina.com>
2017-04-02 19:40:17 +0800

    add NewFileContent NewDescription support file and byte[]

--- Utils.cs
+++ Utils.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace wxUploadMaterial
+namespace WeiXin.Material
 {
     public static class Utils
     {
@@ -32,37 +33,63 @@ namespace wxUploadMaterial
             return String.IsNullOrEmpty(contentType) ? "" : contentType;
         }
 
-        public static string PostFile(string url, string file, string title, string introduction)
+        /// <summary>
+        /// 和微信错误信息的格式保持一致
+        /// </summary>
+        public static string GetErrorMessage(string message)
         {
-            string contentType = GetContentType(file);
+            return String.Format("{{\"errcode\":\"101\", \"errmsg\":\"{0}\"}}", message);
+        }
+
+        public static HttpContent NewFileContent(byte[] contentData, string fileName, string contentType)
+        {
+            ByteArrayContent content = new ByteArrayContent(contentData);
+            content.Headers.ContentType = new MediaTypeHeaderValue(contentType);
+            content.Headers.ContentDisposition = new ContentDispositionHeaderValue("form-data");
+            content.Headers.ContentDisposition.Name = "\"media\"";
+            content.Headers.ContentDisposition.FileName = $"\"{fileName}\"";
+            return content;
+        }
+
+        public static HttpContent NewFileContent(string filePath)
+        {
+            string fileName = Guid.NewGuid().ToString("N") + Path.GetExtension(filePath);
+            string contentType = GetContentType(filePath);
+            return NewFileContent(File.ReadAllBytes(filePath), fileName.ToLower(), contentType);
+        }
+
+        public static HttpContent NewDescription(string title, string introduction)
+        {
+            string description = String.Format("{{\"title\":\"{0}\", \"introduction\":\"{1}\"}}", title, introduction);
+            StringContent content = new StringContent(description, Encoding.UTF8, "application/json");
+            content.Headers.ContentDisposition = new ContentDispositionHeaderValue("form-data");
+            content.Headers.ContentDisposition.Name = "description";
+            return content;
+        }
+
+        /// <summary>
+        /// PostFile(url, NewFileContent(@"d:\test.wav"), NewDescription("Notify", "Bill Create Notify"));
+        /// </summary>
+        public static string PostFile(string url, HttpContent fileContent, HttpContent descriptionContent = null)
+        {
+            string contentType = fileContent.Headers.ContentType.MediaType;
             if (String.IsNullOrEmpty(contentType))
-                return "file type is not supported";
+                throw new Exception(GetErrorMessage("file type is not supported"));
+
             bool isAudioVideo = (contentType.StartsWith("audio") || contentType.StartsWith("video"));
-            string description = "";
             if (isAudioVideo)
             {
-                if (String.IsNullOrEmpty(title))
-                    return "title is null";
-                if (String.IsNullOrEmpty(introduction))
-                    return "introduction is null";
-                description = String.Format("{{\"title\":\"{0}\", \"introduction\":\"{1}\"}}", title, introduction);
+                if (descriptionContent == null)
+                    throw new Exception(GetErrorMessage("description is null"));
             }
 
             string boundary = "----" + DateTime.Now.Ticks.ToString("x");//微信要求boundary的值不可以存在双引号
             MultipartFormDataContent content = new MultipartFormDataContent(boundary);
             content.Headers.Remove("Content-Type");
             content.Headers.TryAddWithoutValidation("Content-Type", "multipart/form-data; boundary=" + boundary);
-            ByteArrayContent dataContent = new ByteArrayContent(File.ReadAllBytes(file));
-            dataContent.Headers.Remove("Content-Type");
-            dataContent.Headers.TryAddWithoutValidation("Content-Type", contentType);
-            dataContent.Headers.Remove("Content-Disposition");
-            dataContent.Headers.TryAddWithoutValidation("Content-Disposition", "form-data; name=\"media\"; filename=\"" + Path.GetFileName(file) + "\"");
-            content.Add(dataContent, "media", "test.wav");
-            if (!String.IsNullOrEmpty(description))
-            {
-                StringContent memoContent = new StringContent(description, Encoding.UTF8, "application/json");
-                content.Add(memoContent, "description");
-            }
+            content.Add(fileContent);
+            if (isAudioVideo)
+                content.Add(descriptionContent);
 
             HttpClient client = new HttpClient();
             Task<HttpResponseMessage> task = client.PostAsync(url, content);
